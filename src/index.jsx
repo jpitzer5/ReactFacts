c8b85a67d6096b0bcc39ff9ed81e95d99791e772
Joel Pitzer <jpitzer5@gmail.com>
2025-01-15 12:11:09 -0800

    extracted React components to separate files

--- src/index.jsx
+++ src/index.jsx
@@ -1,37 +1,11 @@
 // ReactFacts Project from Learn React class on scrimba.com
 
 import { createRoot } from "react-dom/client"
+
+import Header from "./Header"
+import MainContent from "./MainContent"
+
 const root = createRoot(document.getElementById("root"))
-
-function Header() {
-    return (
-        <header className="header">
-            <img src="/src/assets/react-logo.png" className="nav-logo" alt="React logo" />
-            <nav>
-                <ul className="nav-list">
-                    <li>Pricing</li>
-                    <li>About</li>
-                    <li>Contact</li>
-                </ul>
-            </nav>
-        </header>
-    )
-}
-
-function MainContent() {
-    return (
-        <main>
-            <h1>Fun facts about React</h1>
-            <ul>
-                <li>Was first released in 2013</li>
-                <li>Was originally created by Jordan Walke</li>
-                <li>Has well over 100K stars on GitHub</li>
-                <li>Is maintained by Meta</li>
-                <li>Powers thousands of enterprise apps, including mobile apps</li>
-            </ul>
-        </main>
-    )
-}
 
 function Page() {
     return (
